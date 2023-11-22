d5cbf4075eeb2063734215f4f14b0a0eff4fbb89
JumpeiMatsuoka <150517745+JumpeiMatsuoka@users.noreply.github.com>
2023-11-22 10:16:02 +0900

    Update Review01.java

--- Review01.java
+++ Review01.java
@@ -4,16 +4,15 @@ public class Review01 {
     public static void main(String[] args) {
         // TODO 自動生成されたメソッド・スタブ
         
-        int nedan=1500;
+    int nedan=1500;
         double Tax = 0.1;
         int result = tax(nedan,Tax);
         
-        System.out.println(nedan+"の商品の税込価格は"+(nedan+(int)result)+"円（消費税は"+(int)result+"円）です。");
-    }
-            public static int tax(int nedan,double Tax){
-            int result = (int)(nedan*Tax);
-            return result;
-           }
-            }
+        System.out.println(nedan+"円商品の税込価格は"+(nedan+result)+"円（消費税は"+result+"円）です。");
+        }
+        public static int tax(int nedan,double Tax){
+        int result = (int)(nedan*Tax);
+        return result;}
+        }
 
 
